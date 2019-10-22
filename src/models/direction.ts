8f2ec8e76b5213c7dae5b54d0b6727699015aa25
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-10-22 10:52:56 -0500

    feat: bus tracker client

---
+++ src/models/direction.ts
@@ -0,0 +1,6 @@
+export enum Direction {
+    EASTBOUND = 'Eastbound',
+    WESTBOUND = 'Westbound',
+    NORTHBOUND = 'Northbound',
+    SOUTHBOUND = 'Southbound'
+}
--- src/utils/api-key-provider.ts
+++ src/utils/api-key-provider.ts
@@ -1,12 +1,13 @@
-import { config } from 'dotenv';
-import { ApiType } from './api-type';
+import {config} from 'dotenv';
+import {ApiType} from '../models/api-type';
 
 export class ApiKeyProvider {
-    
-    private constructor() {}
-    
-    public static getApiKey(apiType : ApiType) : string | undefined {
+
+    private constructor() {
+    }
+
+    public static getApiKey(apiType: ApiType): string {
         config();
-        return process.env[apiType];
+        return process.env[apiType] || '';
     }
 }
