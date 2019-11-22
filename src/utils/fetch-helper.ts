87a204997e80769bae2b21343b0b9a1a1e67d4c4
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-21 19:04:41 -0600

    feat: webpack
    * remove unused dependencies
    
    * feat: webpack

--- src/utils/fetch-helper.ts
+++ src/utils/fetch-helper.ts
@@ -1,11 +1,9 @@
-import fetch from "node-fetch";
-
 export class FetchHelper {
     private constructor() {
     }
 
     static async fetch<T>(url: URL): Promise<T> {
-        return fetch(url)
+        return fetch(url.toString())
             .then(response => response.json())
             .catch(err => console.error('Error during fetching the url', err));
     }
