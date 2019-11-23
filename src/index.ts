658af7814d88201de83b5d04057b851bde55474d
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-23 11:53:54 -0600

    feat: bus and train tracker views

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,16 @@
 import * as ons from 'onsenui';
-import {TrainTrackerService} from "./services/train-tracker-service";
-import {BusTrackerService} from "./services/bus-tracker-service";
-import {Route} from "./models/route";
-import {Prediction} from "./models/prediction";
+import {BusTrackerView} from "./view/bus-tracker-view";
+import {TrainTrackerView} from "./view/train-tracker-view";
 
-let trainService: TrainTrackerService;
-let busService: BusTrackerService;
+let busTrackerView: BusTrackerView;
+let trainTrackerView: TrainTrackerView;
 
-function init() {
-    trainService = new TrainTrackerService();
-    busService = new BusTrackerService();
-}
-
-async function changePage(service: BusTrackerService | TrainTrackerService, elementId: string) {
-    const navigator = document.querySelector('#navigator') as ons.OnsNavigatorElement;
-    await navigator.resetToPage('templates/bus-predictions.html')
-        .then(() => service.getPredictions('9', '6066'))
-        .then((predictions: Prediction[]) => {
-            const predictionList: Element = document.querySelector(elementId)!;
-            predictions.forEach((prediction: Prediction) => {
-                predictionList.appendChild(ons.createElement(`
-                    <ons-list-item>
-                        ${prediction.arrivalTime} ${prediction.stopId}</ons-button>
-                    </ons-list-item>
-                `) as Node);
-            });
-        });
-}
-
-async function init_routes(service: BusTrackerService | TrainTrackerService, elementId: string) {
-    await service.getRoutes()
-        .then((routes: Route[]) => {
-            const routeList: Element = document.querySelector(elementId)!;
-            routes.forEach((route: Route) => {
-                const listItem = ons.createElement(`
-                    <ons-list-item id="${route.id}">
-                        ${route.id} ${route.name}
-                    </ons-list-item>
-                `) as Node;
-                // listItem.addEventListener('click', async () => await changePage(service, '#bus-prediction-list'));
-                routeList.appendChild(listItem);
-            });
-        });
+async function initViews(): Promise<void[]> {
+    trainTrackerView = new TrainTrackerView();
+    busTrackerView = new BusTrackerView();
+    return Promise.all([trainTrackerView.init(), busTrackerView.init()]);
 }
 
 ons.ready(async () => {
-    init();
-    const promiseList: Promise<void>[] = [];
-    promiseList.push(init_routes(trainService, '#train-route-list'));
-    promiseList.push(init_routes(busService, '#bus-route-list'));
-    await Promise.all(promiseList);
+    await initViews();
 });
