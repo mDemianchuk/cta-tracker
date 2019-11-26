bd786e2055aa3996a4a075117089d732518bead5
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-25 19:23:32 -0600

    feat: stop and station view

--- src/utils/page-helper.ts
+++ src/utils/page-helper.ts
@@ -8,4 +8,10 @@ export class PageHelper {
         const navigator = document.querySelector('#navigator') as ons.OnsNavigatorElement;
         return navigator.bringPageTop(page);
     }
+
+    static async updateTitle(titleName: string, elementId: string): Promise<void> {
+        const title: Element = document.querySelector(elementId)!;
+        title.innerHTML = titleName;
+        return Promise.resolve();
+    }
 }
