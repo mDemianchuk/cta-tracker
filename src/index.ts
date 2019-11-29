f4e148d27a9eb209b7a10ff38b86c7e9c555d3ff
Mykola Demianchuk <nickdemianchuk@gmail.com>
2019-11-29 11:52:19 -0600

    feat: single toolbar

--- src/index.ts
+++ src/index.ts
@@ -18,49 +18,70 @@ function initViews(): void {
 }
 
 function initEventListeners(): void {
-    // on page initialization
     document.addEventListener('init', async event => {
         const page = event.target as ons.OnsPageElement;
         PageHelper.addSpinner(page);
-
-        const toolbar = PageHelper.getTabToolbarByPage(page);
-        const toolbarTitle = toolbar.querySelector('.center') as ons.OnsToolbarElement;
-
-        if (page.data && page.data.title) {
-            toolbarTitle.innerHTML = page.data.title;
-        }
-
         if (!page.matches('#main-page')) {
-            const toolbarLeft = toolbar.querySelector('.left') as ons.OnsToolbarElement;
-            const backButton = ons.createElement(`
-                <ons-back-button></ons-back-button>
-            `) as ons.OnsBackButtonElement;
-            toolbarLeft.appendChild(backButton);
-        }
+            const toolbar = page.querySelector('ons-toolbar') as ons.OnsToolbarElement;
+            const toolbarLeft = page.querySelector('.left') as ons.OnsToolbarElement;
+            const toolbarTitle = toolbar.querySelector('.center') as ons.OnsToolbarElement;
 
-        if (page.matches('#train-route')) {
-            await initTrainRoutes();
-        } else if (page.matches('#bus-route')) {
-            await initBusRoutes();
-        } else if (page.data && page.data.pageId) {
-            const pageId: string = page.data.pageId;
-            if (pageId === 'bus-stop') {
-                await initBusStops(page);
-            } else if (pageId === 'train-station') {
-                await initTrainStations(page);
+            // change title
+            if (page.data && page.data.title) {
+                toolbarTitle.innerHTML = page.data.title;
             }
+
+            // render page content
+            if (page.matches('#train-route')) {
+                await initTrainRoutes();
+            } else if (page.matches('#bus-route')) {
+                await initBusRoutes();
+            } else if (page.data && page.data.pageId) {
+                const pageId: string = page.data.pageId;
+                if (pageId === 'bus-stop') {
+                    await initBusStops(page);
+                } else if (pageId === 'train-station') {
+                    await initTrainStations(page);
+                }
+            }
+
+            // init search
+            // const toolbarRight = toolbar.querySelector('.right') as ons.OnsToolbarElement;
+            // const searchButton = toolbarRight.querySelector('.search') as ons.OnsToolbarElement;
+            // if (toolbarRight && searchButton) {
+            //     const searchBarContainer = ons.createElement(`
+            //         <div style="
+            //             margin: 0;
+            //             position: absolute;
+            //             top: 50%;
+            //             left: 40%;
+            //             transform: translate(-50%, -50%);">
+            //         </div>
+            //     `) as ons.OnsPageElement;
+            //     const searchBar = ons.createElement(`
+            //         <ons-search-input placeholder="Search" style="width: 130%"></ons-search-input>
+            //     `) as ons.OnsSearchInputElement;
+            //
+            //     searchBarContainer.appendChild(searchBar);
+            //
+            //     searchButton.addEventListener('click', () => {
+            //         toolbarLeft.style.display = 'none';
+            //         toolbarTitle.style.display = 'none';
+            //         toolbarRight.style.display = 'none';
+            //         toolbar.appendChild(searchBar);
+            //     });
+            //
+            //     // @ts-ignore
+            //     searchBar.onchange(event => {
+            //         toolbar.removeChild(searchBar);
+            //         toolbarLeft.style.display = 'block';
+            //         toolbarTitle.style.display = 'block';
+            //         toolbarRight.style.display = 'block';
+            //     });
+            // }
         }
         PageHelper.removeSpinner(page);
     });
-
-    // // on tab change
-    // document.addEventListener('prechange', event => {
-    //     const nextTabIndex = PageHelper.getNextTabIndex();
-    //     const mainToolbar = PageHelper.getMainToolbar();
-    //     const nextTabToolbar = PageHelper.getTabToolbarByIndex(nextTabIndex);
-    //     mainToolbar.innerHTML = nextTabToolbar.innerHTML;
-    //     //copy event listeners too
-    // });
 }
 
 async function initTrainRoutes(): Promise<void> {
