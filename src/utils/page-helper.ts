6b8de340f9590e1a6df67efede054aac8590ab50
mDemianchuk <nickdemianchuk@gmail.com>
2019-11-30 17:31:21 -0600

    refactor: remove unused functionality

--- src/utils/page-helper.ts
+++ src/utils/page-helper.ts
@@ -5,46 +5,7 @@ import {Route} from "../models/route";
 import {Prediction} from "../models/prediction";
 
 export class PageHelper {
-    private static tabMap: Map<number, string> = new Map([[0, "train"], [1, "bus"]]);
-
     private constructor() {
-    }
-
-    private static getActiveTabIndex(): number {
-        const tabBar = document.querySelector('ons-tabbar') as ons.OnsTabbarElement;
-        return tabBar.getActiveTabIndex();
-    }
-
-    static getNextTabIndex(): number {
-        const activeTabIndex = PageHelper.getActiveTabIndex();
-        return Math.abs(activeTabIndex - 1);
-    }
-
-    private static getTabTypeByIndex(index: number): string {
-        return PageHelper.tabMap.get(index)!;
-    }
-
-    private static getTabNavigatorByIndex(index: number): ons.OnsNavigatorElement {
-        const tabType: string = PageHelper.getTabTypeByIndex(index);
-        return PageHelper.getTabNavigatorByType(tabType);
-    }
-
-    static getTabNavigatorByType(tabType: string): ons.OnsNavigatorElement {
-        return document.querySelector(`#${tabType}-navigator`) as ons.OnsNavigatorElement;
-    }
-
-    static getMainToolbar(): ons.OnsToolbarElement {
-        return document.querySelector('#main-toolbar') as ons.OnsToolbarElement;
-    }
-
-    static getTabToolbarByIndex(index: number): ons.OnsToolbarElement {
-        const tabNavigator = PageHelper.getTabNavigatorByIndex(index);
-        const topPage = tabNavigator.topPage;
-        return PageHelper.getTabToolbarByPage(topPage);
-    }
-
-    static getTabToolbarByPage(page: HTMLElement): ons.OnsToolbarElement {
-        return page.querySelector('ons-toolbar') as ons.OnsToolbarElement;
     }
 
     static createRouteListElement(route: Route, eventListener: EventListener): ons.OnsListItemElement {
